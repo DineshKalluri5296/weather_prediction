02709174e3d28d5d17698ca9fb2f9bb3c28d7ddb
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-02-23 13:01:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from mlflow.tracking import MlflowClient
 import joblib
 
 mlflow.set_tracking_uri("http://100.54.111.135:5000")   
-mlflow.set_experiment("Seattle_weather12")
+mlflow.set_experiment("Seattle_weather123")
 
 df = pd.read_csv("seattle-weather.csv")
 df = df.dropna()
