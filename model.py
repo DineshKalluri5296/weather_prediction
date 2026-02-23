1af091d61527c23aef4deb9091cbe17c5e675b95
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-02-23 13:24:41 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 # from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import mlflow.sklearn
@@ -24,13 +24,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 with mlflow.start_run() as run:
 
-    model = RandomForestClassifier(
-        n_estimators=100,
-        max_depth=10,
-        random_state=42
-    )
+    # model = RandomForestClassifier(
+    #     n_estimators=100,
+    #     max_depth=10,
+    #     random_state=42
+    # )
     # model=LogisticRegression()
-
+    model=DecisionTreeClassifier()
     model.fit(X_train, y_train)
 
     pred = model.predict(X_test)
@@ -60,7 +60,7 @@ latest_version = client.get_latest_versions("SeattleWeatherModel5")[0].version
 client.update_model_version(
     name="SeattleWeatherModel5",
     version=latest_version,
-    description="Randomforestclassifier model trained on Seattle weather dataset"
+    description="DecisionTreeClassifier model trained on Seattle weather dataset"
 )
 
 print(f"Model Version {latest_version} updated with description successfully!")
