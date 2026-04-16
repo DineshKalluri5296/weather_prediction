0b2a233f1729cd4fd54da42d520240302b7c99d7
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-04-16 11:07:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 with mlflow.start_run() as run:
 
-     model = RandomForestClassifier(
+    model = RandomForestClassifier(
         n_estimators=100,
         max_depth=10,
         random_state=42
