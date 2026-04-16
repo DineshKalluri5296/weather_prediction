cb994513f3858dbdefb58f0724e8b3998cb672e4
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-04-16 20:52:12 +0530

    Update model.py

--- model.py
+++ model.py
@@ -25,10 +25,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 with mlflow.start_run() as run:
 
     # model = RandomForestClassifier(
-        n_estimators=100,
-        max_depth=10,
-        random_state=42
-    )
+        # n_estimators=100,
+        # max_depth=10,
+        # random_state=42
+    # )
     # model=LogisticRegression()
     model=DecisionTreeClassifier()
     model.fit(X_train, y_train)
